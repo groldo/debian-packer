903d15f83fd83e525741821c5447ef977fd368d7
groldo <groldo@dumpme.de>
2023-07-02 16:17:10 +0200

    fixed indention

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,13 +2,13 @@
 
 apt-get update
 apt-get install -y vim gnome-core \
-			openvpn \
-			resolvconf \
-                	open-vm-tools-desktop \
-			firefox-esr \
-			chromium \
-			adwaita-icon-theme-full \
-			yaru-theme-gtk \
-			python3 \
-			python3-pip \
-			python3-venv
+						openvpn \
+						resolvconf \
+						open-vm-tools-desktop \
+						firefox-esr \
+						chromium \
+						adwaita-icon-theme-full \
+						yaru-theme-gtk \
+						python3 \
+						python3-pip \
+						python3-venv
